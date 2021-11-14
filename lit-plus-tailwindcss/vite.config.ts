18c64facada5bb268944b0acd2c1ce1833b51c77
Phillip Baird <phillip.baird@gmail.com>
2021-11-14 14:11:26 +1300

    Fix Vite build config.

--- lit-plus-tailwindcss/vite.config.ts
+++ lit-plus-tailwindcss/vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src'
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
 })
