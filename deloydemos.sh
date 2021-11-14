df4142ce3639446d13e2bbc98efcdf991b56ee7d
Phillip Baird <phillip.baird@gmail.com>
2021-11-14 14:11:49 +1300

    Add new demo to demo deployment.

--- deloydemos.sh
+++ deloydemos.sh
@@ -7,7 +7,13 @@ cd thinking-in-lit
 npm run build
 cp -r dist/* ../dist/thinking-in-lit
 sed -i 's/\/assets/assets/g' ../dist/thinking-in-lit/index.html
+cd ..
 
+mkdir dist/lit-plus-tailwindcss
+cd lit-plus-tailwindcss
+npm run build
+cp -r dist/* ../dist/lit-plus-tailwindcss
+sed -i 's/\/assets/assets/g' ../dist/lit-plus-tailwindcss/index.html
 cd ..
 
 npm run deploy
